267fa4a9b743c216ec68ffabc7ece698391c59f1
dhdhk <124845236+dhdhk@users.noreply.github.com>
2023-08-04 16:48:45 +0900

    memo
    dfdfdf

---
+++ static/app.js
@@ -0,0 +1,38 @@
+function displayMemo(memos) {
+  const ul = document.querySelector("#memo-ul");
+  const li = document.querySelector("li");
+  li.innerText = `[id:${momo.id}] ${memo.content}`;
+  ul.appendChild(li);
+}
+
+async function readMemo() {
+  const res = await fetch("/memos");
+  const jsonRes = await res.json();
+  jsonRes.forEach(displayMemo);
+}
+
+async function createMemo(value) {
+  const res = await fetch("/memos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: new Date(),
+      content: value,
+    }),
+  });
+  readMemo();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = document.querySelector("#memo-input");
+  createMemo(input.value);
+  input.value = "";
+}
+
+readMemo();
+
+const form = document.querySelector("#memo-form");
+form.addEventListener("submit", createMemo);
